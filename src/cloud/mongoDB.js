c9e2130fec62493518ce9ac82fff735efa22c31d
trekof <118328676+trekof@users.noreply.github.com>
2025-04-16 10:29:49 +0700

    Update mongoDB.js

--- src/cloud/mongoDB.js
+++ src/cloud/mongoDB.js
@@ -4,10 +4,10 @@ mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
 }).then(() => {
-    console.log('\x1b[32m%s\x1b[0m', 'Connected to MongoDB successfully!');  // Hiển thị màu xanh nếu kết nối thành công
+    console.log('\x1b[32m%s\x1b[0m', 'Connected to MongoDB successfully!');  
 }).catch(err => {
-    console.error('\x1b[31m%s\x1b[0m', `Error connecting to MongoDB: ${err.message}`); // Hiển thị màu đỏ nếu kết nối thất bại
-    process.exit(1);  // Dừng ứng dụng nếu không thể kết nối đến MongoDB
+    console.error('\x1b[31m%s\x1b[0m', `Error connecting to MongoDB: ${err.message}`);
+    process.exit(1);  
 });
 
 module.exports = mongoose;
