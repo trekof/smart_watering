c745f04f762064377691abec66a719566c483cc7
trekof <fokertfoketr@gmail.com>
2025-04-16 10:28:19 +0700

    change mongo

--- src/cloud/mongoDB.js
+++ src/cloud/mongoDB.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
-    console.log('Connected to Mongo DB')
-    useNewUrlParser: true;
-    useUnifiedTopology: true;
-}).catch(err => console.log(err))
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('\x1b[32m%s\x1b[0m', 'Connected to MongoDB successfully!');  // Hiển thị màu xanh nếu kết nối thành công
+}).catch(err => {
+    console.error('\x1b[31m%s\x1b[0m', `Error connecting to MongoDB: ${err.message}`); // Hiển thị màu đỏ nếu kết nối thất bại
+    process.exit(1);  // Dừng ứng dụng nếu không thể kết nối đến MongoDB
+});
 
 module.exports = mongoose;
