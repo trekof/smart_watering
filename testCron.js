90a382527a1e247bac4954c274ad015e66ede39f
trekof <fokertfoketr@gmail.com>
2025-04-19 19:51:12 +0700

    cron sec

--- testCron.js
+++ testCron.js
@@ -3,10 +3,10 @@ const cron = require('node-cron');
 cron.schedule('* * * * *', () => {
   console.log(`[${new Date().toLocaleTimeString()}] Starting 6 tasks with 10s interval:`);
 
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 60; i++) {
     setTimeout(() => {
       const time = new Date().toLocaleTimeString();
       console.log(`  [${time}] Task ${i + 1}`);
-    }, i * 10000); // 0s, 10s, 20s, 30s, 40s, 50s
+    }, i * 1000);
   }
 });
